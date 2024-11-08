e58ec9e609382ca79ecd9c4ee4e27e4361251e8f
Leonardo Takehana <leonardotakehana@gmail.com>
2024-11-08 14:56:26 -0300

    2046:Create a initial structure to code

---
+++ 2046.SecondMinimumTimeToReachDestination.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int secondMinimum(int n, int** edges, int edgesSize, int* edgesColSize, int time, int change) {
+ return 0;   
+}
+
+
+int main(){
+    return 0;
+}
