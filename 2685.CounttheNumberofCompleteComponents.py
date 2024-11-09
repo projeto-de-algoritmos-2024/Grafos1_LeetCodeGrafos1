d47078b2b9469a63e5ed51d9b38302201b08e4ea
phrezende-eng <ph.hr.001@gmail.com>
2024-11-09 02:36:51 -0300

    2685: use is_complete_component to count complete components

--- 2685.CounttheNumberofCompleteComponents.py
+++ 2685.CounttheNumberofCompleteComponents.py
@@ -1,43 +1,37 @@
 class Solution(object):
-    queue = []
-    explored = []
-    visited = []
     count_components = 0
-    graph = {}
 
-    def bfs_all_components(self, adj):
-        for node in adj:
-            if node in self.explored:
-                # print(f"Nó: {node} já foi explorado")
-                continue
-            self.count_components += 1
-            self.bfs(adj, node)
-        return self.explored
-
-    def bfs(self, adj, start=None, end=None):
-        self.queue.append(start)
-        self.visited.append(start)
-        while self.queue:
-            node = self.queue.pop(0)
-            for neighbor in adj[node]:
-                if neighbor == end:
-                    return self.explored
-                if neighbor not in self.explored:
-                    self.explored.append(neighbor)
-                    self.visited.append(neighbor)
-                    self.queue.append(neighbor)
-
-        return self.explored
-
-    def is_complete(self, component, adj):
+    def is_complete_component(self, component, adj):
         for node in component:
             if len(adj[node]) != len(component) - 1:
                 return False
         return True
 
+    def bfs(self, adj, start, explored):
+        queue = [start]
+        component = [start]
+        explored.add(start)
+
+        while queue:
+            node = queue.pop(0)
+            for neighbor in adj[node]:
+                if neighbor not in explored:
+                    explored.add(neighbor)
+                    component.append(neighbor)
+                    queue.append(neighbor)
+        return component
+
+    def bfs_all_components(self, adj):
+        explored = set()
+
+        for node in adj:
+            if node not in explored:
+                component = self.bfs(adj, node, explored)
+                if self.is_complete_component(component, adj):
+                    self.count_components += 1
+
     def countCompleteComponents(self, n, edges):
-        if len(edges) == 0:
-            return n
+        self.count_components = 0
 
         graph = {i: [] for i in range(n)}
         for u, v in edges:
@@ -50,20 +44,8 @@ class Solution(object):
 
 def main():
     solution = Solution()
-    result = solution.countCompleteComponents(2, [[1, 0]])
-    # result == 3
-    # [[0, 1],[0, 2],[1, 2],[3, 4]]
-    # graph[0] = [1, 2]
-    # graph[1] = [0, 2]
-    # graph[2] = [0, 1]
-    # #
-    # graph[3] = [4]
-    # graph[4] = [3]
-    # #
-    # graph[5] = []
-    # bfs_all_components(graph)
+    result = solution.countCompleteComponents(6, [[0, 1], [0, 2], [1, 2], [3, 4]])
     print(result)
-    pass
 
 
 main()
